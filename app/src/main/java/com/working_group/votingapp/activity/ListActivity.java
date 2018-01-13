3dca970d814179e3df3a8c6b904842e4130da605
shoooya <shoooya17@gmail.com>
2018-01-13 17:50:36 +0900

    データ取得ロジック追加 (#32)
    * Apiリクエスト処理を追加
    
    * 不用ファイル削除

--- app/src/main/java/com/working_group/votingapp/activity/ListActivity.java
+++ app/src/main/java/com/working_group/votingapp/activity/ListActivity.java
@@ -4,7 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.working_group.votingapp.Datasources.Api.ApiClient;
 import com.working_group.votingapp.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -13,6 +22,13 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("一覧");
         setContentView(R.layout.activity_list);
+        ApiClient api = new ApiClient(new ApiClient.AsyncCallback() {
+            // 実行後 
+            public void postExecute(JSONObject result) {
+                viewEnqueteList(result);
+            }
+        });
+        api.get("http://weather.livedoor.com/forecast/webservice/json/v1?city=400040");
     }
 
     /**
@@ -41,4 +57,23 @@ public class ListActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplication(), AnswerActivity.class);
         startActivity(intent);
     }
+
+    private void viewEnqueteList(JSONObject result) {
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        try {
+            JSONArray forecasts = result.getJSONArray("forecasts");
+
+            for (int i=0; i<forecasts.length(); i++) {
+                JSONObject json = forecasts.getJSONObject(i);
+                String date = json.getString("date");
+                arrayAdapter.add(date);
+            }
+            listView.setAdapter(arrayAdapter);
+        } catch(JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
