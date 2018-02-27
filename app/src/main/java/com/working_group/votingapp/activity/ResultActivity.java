71823be61a8291420d3aa677dc2f4d0af2499d06
stoias <pddybrd.s@gmail.com>
2018-02-28 00:59:39 +0900

    add get result

--- app/src/main/java/com/working_group/votingapp/activity/ResultActivity.java
+++ app/src/main/java/com/working_group/votingapp/activity/ResultActivity.java
@@ -4,10 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.working_group.votingapp.Datasources.Api.ApiClient;
 import com.working_group.votingapp.R;
+import com.working_group.votingapp.entities.Enquete;
+import com.working_group.votingapp.entities.Result;
+import com.working_group.votingapp.libs.EnqueteListAdapter;
+import com.working_group.votingapp.libs.ResultListAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -21,6 +32,14 @@ public class ResultActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         //TODO: intentからアンケートIDを受け取理、APIから結果を取得する
+        int enqueteId = intent.getIntExtra("enqueteId",0);
+        ApiClient api = new ApiClient(new ApiClient.AsyncCallback() {
+            // 実行後 
+            public void postExecute(JSONObject result) {
+                viewResult(result);
+            }
+        });
+        api.get("http://saitos.wmsol.info:56843/answers/" + enqueteId);
 
         //TODO: アンケート結果表示（仮）
         TextView title = (TextView) findViewById(R.id.title);
@@ -32,5 +51,45 @@ public class ResultActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void viewResult(JSONObject answers){
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        final ResultListAdapter adapter = new ResultListAdapter(this);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Result result = adapter.getItem(position);
+                switch (view.getId()) {
+                    //ボタン追加したら書くところ？
+//                    case R.id.answer_button:
+//                        sendAnswerPage(enquete);
+//                        break;
+//                    case R.id.result_button:
+//                        sendResultPage(enquete);
+//                        break;
+                }
+            }
+        });
+
+        try {
+            JSONArray answersJson = answers.getJSONArray("answers");
+
+            for (int i=0; i<answersJson.length(); i++) {
+                JSONObject json = answersJson.getJSONObject(i);
+
+                int itemId = json.getInt("item_id");
+                String comment = json.getString("comment");
+
+                Result result = new Result();
+                result.setItemId(itemId);
+                result.setComment(comment);
+                adapter.add(result);
+            }
+            listView.setAdapter(adapter);
+        }catch(JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
